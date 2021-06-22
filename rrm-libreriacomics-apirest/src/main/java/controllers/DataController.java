65b4fae79e435cd884a87db86043e6fb26d735dd
Romalde1 <rubenromalde1@hotmail.com>
2021-06-22 06:27:15 +0200

    beoñijuvbnoij

--- rrm-libreriacomics-apirest/src/main/java/controllers/DataController.java
+++ rrm-libreriacomics-apirest/src/main/java/controllers/DataController.java
@@ -1,11 +1,9 @@
 package controllers;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 public class DataController {
 
